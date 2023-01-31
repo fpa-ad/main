ee66705bb1c1b2ef9d3172a005d42532383edf5d
084intheImpala <camoessousa@gmail.com>
2023-01-31 19:49:23 +0000

    working

--- src/utils.C
+++ src/utils.C
@@ -1,1 +1,13 @@
-#import "utils.h"
+#include "utils.h"
+#include <sstream>
+
+using namespace std;
+
+string current_time()
+{
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+    stringstream ss;
+    ss << std::put_time(&tm, "%d-%m-%Y %H-%M-%S");
+    return ss.str();
+}
