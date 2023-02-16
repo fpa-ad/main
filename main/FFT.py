f4966086a0d7c1e7b524c181bbe1b8c0b241f895
084intheImpala <camoessousa@gmail.com>
2023-02-16 10:49:39 +0000

    workin

--- main/FFT.py
+++ main/FFT.py
@@ -66,29 +66,30 @@ if os.path.exists("output") and os.path.isdir("output"):
 
         ################ FFT ##############
         #print(PHI)
-        print(PHI[1][1][0])
+        #print(PHI[1][1][0])
         nt=len(timebase)
         for i in range(ny):
-            wave=np.zeros([int(nt),nx])
+            wave=np.zeros([int(nt)-1,nx])
             """for j in range(nt):
                 waveaux=np.zeros(nx)
                 for k in range(nx):
                     waveaux[k]=(PHI[j][k][i])
                 wave[j,:]=waveaux[:]"""
 
-            for j in range(nt):
+            for j in range(1,nt):
                 waveaux=np.zeros(nx)
                 for k in range(nx):
-                    waveaux[k]=(PHI[j][k][i])
+                    waveaux[k]=(PHI[j-1][k][i])
                 if(1):
-                    wave[int(j),:]=waveaux[:]
+                    wave[int(j)-1,:]=waveaux[:]
             
-            wind=np.hanning(int(nt))
+            wind=np.hanning(int(nt)-1)
+            wave2=wave
             #print(wave[1,1])
             for k in range(nx):
                 wave[:,k]*=wind
 
-            print(dx)
+            #print(dx)
             #wave=np.transpose(wave)
             """for k in range(1,nx-1):
                 wave[:,k]=(wave[:,k+1]-wave[:,k-1])
@@ -99,19 +100,87 @@ if os.path.exists("output") and os.path.isdir("output"):
             k_max = np.pi / dx
             omega_max = np.pi / (dt)
 
+            #print(len(wave))
+            print(wave2[100,100])
+            gammaMax=4001
+            spIMAG=np.imag(np.fft.fft2(wave[:gammaMax,:]))
             sp = np.abs(np.fft.fft2(wave))**2
             sp = np.fft.fftshift(sp)
+            spIMAG=np.fft.fftshift(spIMAG)
+
             #print(wave[1,1])
-            plt.imshow( sp, origin = 'lower', norm=colors.LogNorm(vmin=5000),extent = ( -k_max, k_max, -omega_max, omega_max ),aspect = 'auto', cmap = 'gray')
+            plt.imshow( sp, origin = 'lower', norm=colors.LogNorm(vmin=10000), extent = ( -k_max, k_max, -omega_max, omega_max ),aspect = 'auto', cmap = 'gray')
 
             k = np.linspace(-k_max, k_max, num = 512)
-            w=np.sqrt(1)+3/2*(1**2)*(k)**2
-            plt.plot( k, w, label = "Electron Plasma Wave", color = 'r',ls = '-.' )
+            w=np.sqrt(1+1/5)+3/2*(1**2)*(k)**2
+            wce=1/np.sqrt(5)+0*k
+            wce2=2/np.sqrt(5)+0*k
+            wce3=3/np.sqrt(5)+0*k
+            wce4=4/np.sqrt(5)+0*k
+            plt.plot( k, w, label = "Electron Plasma Wave", color = 'r',ls = '--' )
+            plt.plot( k, wce, label = "Electron Plasma Wave", color = 'r',ls = '--' )
+            plt.plot( k, wce2, label = "Electron Plasma Wave", color = 'r',ls = '--' )
+            plt.plot( k, wce3, label = "Electron Plasma Wave", color = 'r',ls = '--' )
+            plt.plot( k, wce4, label = "Electron Plasma Wave", color = 'r',ls = '--' )
 
-            plt.ylim(0,10)
+            plt.ylim(0,3)
             plt.xlim(0,5)
-            plt.xlabel("$k$ [$\omega_n/c$]")
-            plt.ylabel("$\omega$ [$\omega_n$]")
+            plt.xlabel("$k$ [$\lambda_{De}^{-1}$]")
+            plt.ylabel("$\omega$ [$\omega_{pe}$]")
             plt.title("Wave dispersion relation")
 
             plt.savefig("output/"+sim+"/fft_"+str(i)+".png")
+            plt.close()
+
+            Nw=len(sp)
+            Nk=len(np.transpose(sp))
+            Wq=np.zeros(Nk*Nw)
+            Vq=np.zeros(Nk*Nw)
+
+            #print(Nw)
+            #print(Nk)
+
+            for i in range(Nw):
+                for j in range (Nk):
+                    Wq[j+Nk*i]=sp[i][j]
+                    if((-k_max+j*2*k_max/Nk)!=0):
+                        Vq[j+Nk*i]=(-omega_max+i*2*omega_max/Nw)/(-k_max+j*2*k_max/Nk)
+            
+            plt.xlim(0,5)
+            plt.scatter( Vq, Wq, s=1)
+            ax=plt.gca()
+            ax.set_yscale('log')
+            plt.savefig("output/"+sim+"/WQuasi.png")
+            plt.close()
+
+            spimag=np.zeros(Nk)
+            for i in range(gammaMax):
+                #spimag[:]+=np.sqrt(spIMAG[i,:])
+                spimag[:]+=spIMAG[i,:]
+
+
+            #spimag/=Nk
+            spimag/=(dx*dt)
+
+            normAux=np.zeros([nt,nx,ny])
+
+            for i in range(ny):
+                for j in range(nt):
+                    for k in range(nx):
+                        normAux[j,k,i]=PHI[j][k][i]
+            
+            normPhi=np.amax(normAux[:,:,0])
+            #print(normPhi)
+
+            #spimag/=normPhi
+            #spimag/=timebase[-1]
+            
+            kimag=np.linspace(-k_max,k_max,num = Nk)
+
+            plt.plot(kimag, spimag)
+            plt.xlim(0,1)
+            #plt.xmin(0)
+            plt.title("Two-Stream Growth Rate")
+            plt.xlabel("$k$ [$\lambda_{De}^{-1}$]")
+            plt.ylabel("$\gamma$ [$\omega_{pe}$]")            
+            plt.savefig("output/"+sim+"/fft_imag.png")
--- main/plots.py
+++ main/plots.py
@@ -1,12 +1,11 @@
 import os
 import matplotlib.pyplot as plt
-import matplotlib.colors as colors
-import numpy as np
 import imageio as imageio
 from matplotlib.ticker import (MultipleLocator)
+import matplotlib
 
-minv = 5
-binw = 0.05
+binn = 100
+binr = 5
 
 if os.path.exists("output") and os.path.isdir("output"):
     sims = os.listdir("output")
@@ -18,31 +17,51 @@ if os.path.exists("output") and os.path.isdir("output"):
         files.sort()
         # frames for the gif
         frames = []
-        PHI=[]
-        timebase=[]
-        xbase=[]
-        ybase=[]
-        # read basic info
+        n = 0
         with open("output/"+sim+"/README.txt") as f:
             header = f.readline().split()
             Lx = float(header[1])
             Ly = float(header[3])
             dx = float(header[5])
             dy = float(header[7])
+            n = int(f.readline().split()[0])
+        fig, ax = plt.subplots()
+        plt.xlim(0, Lx)
+        plt.ylim(0, Ly)
+        #ax.xaxis.set_minor_locator(MultipleLocator(dx))
+        #ax.yaxis.set_minor_locator(MultipleLocator(dy))
+        plt.grid(True, which='both')
+        points = None
+        fig2, axs2 = plt.subplots(1, 2, sharey=True, tight_layout=True, figsize=(8,4))
+        hist1 = None
+        hist2 = None
+        c = matplotlib.cm.rainbow
         for file in files:
             if not ".txt" in file:
                 continue
             if (file == "README.txt"):
                 continue
             print(file)
-            fig, ax = plt.subplots()
-            phi_t=[]
             with open("output/"+sim+"/"+file) as f:
+                # clear the plots
+                if points != None:
+                    for p in points:
+                        p.remove()
+                points = []
+                if hist1 != None:
+                    for bar in hist1:
+                        for b in bar:
+                            b.remove()
+                hist1 = []
+                if hist2 != None:
+                    for bar in hist2:
+                        for b in bar:
+                            b.remove()
+                hist2 = []
                 # read time and number of particle types
                 header = f.readline().split()
                 t = float(header[0])
                 n = int(header[2])
-                timebase.append(t)
                 ctm = []
                 # for each particle type
                 for i in range(n):
@@ -59,31 +78,29 @@ if os.path.exists("output") and os.path.isdir("output"):
                         y.append(float(line[1]))
                         vx.append(float(line[2]))
                         vy.append(float(line[3]))
-                    #print(x)
-                    #print(y)
-                    plt.scatter(x, vx, label="q/m="+header[0],s=1)
-            plt.xlim(0, Lx)
-            plt.ylim(-10, 10)
-            ax.xaxis.set_minor_locator(MultipleLocator(dx*10))
-            ax.yaxis.set_minor_locator(MultipleLocator(dy*10))
-            plt.grid(True, which='both')
-            plt.title(f"t={t}s")
+                    if (n == 1):
+                        col = 0
+                    else:
+                        col = i / (n-1)
+                    plt.figure(1)
+                    points.append(plt.scatter(x, y, label="q/m="+header[0], s=10, color=c(col)))
+                    plt.figure(2)
+                    counts, bins, bars1 = axs2[0].hist(vx, bins = binn, range = (-binr, binr), label="q/m="+header[0], alpha=0.5, color=c(col))
+                    hist1.append(bars1)
+                    counts, bins, bars2 = axs2[1].hist(vy, bins = binn, range = (-binr, binr), label="q/m="+header[0], alpha=0.5, color=c(col))
+                    hist2.append(bars2)
+            plt.figure(1)
             plt.legend()
+            plt.title(f"t={t}s")
             plt.savefig("output/"+sim+"/"+file.replace("txt","png"))
             frames.append(int(file.replace(".txt","")))
-            #plt.show()
-            plt.close(fig)
-            fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True, figsize=(8,4))
-            axs[0].hist(vx, bins=np.arange(-minv, minv, binw))
-            axs[0].set(title=f"$v_x$ for t={t}s")
-            #axs[0].yaxis.set_major_locator(MultipleLocator(1))
-            axs[1].hist(vy, bins=np.arange(-minv, minv, binw))
-            axs[1].set(title=f"$v_y$ for t={t}s")
+            plt.figure(2)
+            axs2[0].set(title=f"$v_x$ for t={t}s")
+            plt.legend()
+            axs2[1].set(title=f"$v_y$ for t={t}s")
+            plt.legend()
             plt.savefig("output/"+sim+"/"+file.replace(".txt","")+"_hist.png")
-            #plt.show()
-            plt.close(fig)
             
-
         frames.sort()
         with imageio.get_writer("output/"+sim+"/"+'sim.gif', mode='I', fps=10) as writer:
             for filename in frames:
@@ -97,37 +114,3 @@ if os.path.exists("output") and os.path.isdir("output"):
                 print(name)
                 image = imageio.imread(name)
                 writer.append_data(image)
-
-        """################ FFT ##############
-        #print(PHI)
-        print(PHI[1][1][0])
-        nt=frames[-1]
-        for i in range(ny):
-            wave=np.zeros([nt,nx])
-            for j in range(nt):
-                waveaux=np.zeros(nx)
-                for k in range(nx):
-                    waveaux[k]=(PHI[j][k][i])
-                wave[j,:]=waveaux[:]
-            
-            wind=np.hanning(nt)
-            print(wave[1,1])
-            for k in range(nx):
-                wave[:,k]*=wind
-
-            dt=timebase[1]-timebase[0]
-            k_max = np.pi / dx
-            omega_max = np.pi / dt
-
-            sp = np.abs(np.fft.fft2(wave))**2
-            sp = np.fft.fftshift(sp)
-            print(wave[1,1])
-            plt.imshow( sp, origin = 'lower', norm=colors.LogNorm(),extent = ( -k_max, k_max, -omega_max, omega_max ),aspect = 'auto', cmap = 'gray')
-
-            plt.ylim(0,omega_max)
-            plt.xlim(0,k_max)
-            plt.xlabel("$k$ [$\omega_n/c$]")
-            plt.ylabel("$\omega$ [$\omega_n$]")
-            plt.title("Wave dispersion relation")
-
-            plt.savefig("output/"+sim+"/fft_"+str(i)+".png")"""
